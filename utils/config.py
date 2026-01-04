1f97be1460c3147c0fa38879ba1d5a46430de2d0
Chirag Rane <beingchirag7@gmail.com>
2026-01-04 14:24:15 +0530

    AI Explanation Removed

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     MONGODB_URL: str
     MONGODB_DB_NAME: str = "fintech_db"
-    GEMINI_AI_API_KEY: str
     
 
     class Config:
